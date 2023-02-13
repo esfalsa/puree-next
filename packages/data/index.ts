c0bf585e66d4c4b40ef44049f22ef66bbdd9a81a
esfalsa <pronoun.nationstates@gmail.com>
2023-02-12 21:51:19 -0800

    feat(data): add basic functionality

---
+++ packages/data/index.ts
@@ -0,0 +1,58 @@
+import { pipeline } from "node:stream/promises";
+import { createGunzip } from "node:zlib";
+import { request } from "undici";
+import { Parser, RegionSaver } from "archiver";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const userAgent = "Region Archiver/0.1.0 (by: Esfalsa)";
+const imgbbKey = process.env["IMGBB_KEY"];
+
+if (!imgbbKey) {
+  throw new Error("No imgBB key found.");
+}
+
+const { body } = await request(
+  "https://esfalsa.github.io/puree/regions.xml.gz",
+  {
+    headers: {
+      "user-agent": userAgent,
+    },
+  }
+);
+
+const passworded: string[] =
+  (
+    await request(
+      "https://www.nationstates.net/cgi-bin/api.cgi?q=regionsbytag;tags=password",
+      {
+        headers: {
+          "user-agent": userAgent,
+        },
+      }
+    ).then(({ body }) => body.text())
+  )
+    .split("<REGIONS>")[1]
+    ?.split("</REGIONS>")[0]
+    ?.split(",") ?? [];
+
+const timer = new AbortController();
+
+// abort after one hour
+setTimeout(() => {
+  timer.abort();
+}, 60 * 60 * 1000);
+
+pipeline(
+  body,
+  createGunzip(),
+  new Parser({
+    filter: ({ name, delegateAuth }) =>
+      Boolean(delegateAuth?.includes("X") && !passworded.includes(name)),
+  }),
+  new RegionSaver({ userAgent, imgbbKey }),
+  {
+    signal: timer.signal,
+  }
+);
